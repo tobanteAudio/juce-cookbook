46928871bd4d170a957fa37e3d85725c2ec99cc5
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-06-06 12:16:15 +0200

    Move opengl examples to subdirectory

---
+++ opengl/opengl-shader/Source/MainComponent.cpp
@@ -0,0 +1,56 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    // Make sure you set the size of the component after
+    // you add any child components.
+    setSize (800, 600);
+}
+
+MainComponent::~MainComponent()
+{
+    // This shuts down the GL system and stops the rendering calls.
+    shutdownOpenGL();
+}
+
+//==============================================================================
+void MainComponent::initialise()
+{
+    // Initialise GL objects for rendering here.
+}
+
+void MainComponent::shutdown()
+{
+    // Free any GL objects created for rendering here.
+}
+
+void MainComponent::render()
+{
+    // This clears the context with a black background.
+    OpenGLHelpers::clear (Colours::black);
+
+    // Add your rendering code here...
+}
+
+//==============================================================================
+void MainComponent::paint (Graphics& g)
+{
+    // You can add your component specific drawing code here!
+    // This will draw over the top of the openGL background.
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
---
+++ opengl/opengl-shader/Source/MainComponent.h
@@ -0,0 +1,40 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent   : public OpenGLAppComponent
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent();
+
+    //==============================================================================
+    void initialise() override;
+    void shutdown() override;
+    void render() override;
+
+    //==============================================================================
+    void paint (Graphics& g) override;
+    void resized() override;
+
+private:
+    //==============================================================================
+    // Your private member variables go here...
+
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
